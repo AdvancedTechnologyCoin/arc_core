fe5b390bc44494fbdab9f61a32f269c91f0e543b
EIDOSO <wericjohn280@gmail.com>
2019-11-01 20:03:51 +0100

    change end spork id to 21

--- src/spork.h
+++ src/spork.h
@@ -18,7 +18,7 @@ class CEvolutionManager;
     - This would result in old clients getting confused about which spork is for what
 */
 static const int SPORK_START                                            = 10001;
-static const int SPORK_END                                              = 10013;
+static const int SPORK_END                                              = 10021;
 
 static const int SPORK_2_INSTANTSEND_ENABLED                            = 10001;
 static const int SPORK_3_INSTANTSEND_BLOCK_FILTERING                    = 10002;
